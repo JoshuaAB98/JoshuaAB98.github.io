8b8907104b9ba0e5e86fa8b83591d7dece35ae19
JoshuaAB98 <joshnash1998@gmail.com>
2020-12-23 18:50:51 +0000

    Need to fix objs stacking

--- main.js
+++ main.js
@@ -1,34 +1,28 @@
 AFRAME.registerComponent('initfunc', {
     init: async function () {
         this.loaded = false;
-        console.log("Pre if statement")
         window.addEventListener('gps-camera-update-position', async (e) => {
             if (this.loaded === false) {
                 this.loaded = true;
                 alert(`Your initial location is: ${e.detail.position.longitude} ${e.detail.position.latitude}`);
-                console.log("Post if statement")
                 this.models = {
                     cafe: ['#coffee-cup', 15],
                     bar: ['#beer-bottle', 0.3],
                     restaurant: ['#burger', 0.02]
                 };
 
-                const res = await fetch('https://hikar.org/course/imm/world.php');
-                // const res = await fetch(`https://www.hikar.org/fm/ws/bsvr.php?bbox=-2.351,50.440,-2.551,50.640&outProj=4326&format=json&poi=all`);
+                // const res = await fetch('https://hikar.org/course/imm/world.php');
+                const res = await fetch(`https://www.hikar.org/fm/ws/bsvr.php?bbox=${e.detail.position.longitude-0.1},${e.detail.position.latitude-0.1},${e.detail.position.longitude+0.1},${e.detail.position.latitude+0.1}&outProj=4326&format=json&poi=all`);
                 const json = await res.json();
 
-                console.log("Before forEach loop")
-
-                json.forEach(obj => {
+                json.features.forEach(obj => {
                     const entity = document.createElement('a-entity');
                     const model = document.createElement('a-entity');
 
-                    console.log("Name: " + obj.name + " X: " + obj.x + " Y: " + obj.y + " Z: " + obj.z + " Type: " + obj.type)
-
-                    if (obj.type == "cafe") {
+                    if (obj.properties.featuretype == "cafe" && obj.properties.name != undefined) {
                         model.setAttribute('obj-model', {
-                            obj: this.models[obj.type][0],
-                            mtl: `${this.models[obj.type][0]}-mtl`
+                            obj: this.models[obj.properties.featuretype][0],
+                            mtl: `${this.models[obj.properties.featuretype][0]}-mtl`
                         });
                         model.setAttribute('position', {
                             x: -2.2,
@@ -36,67 +30,27 @@ AFRAME.registerComponent('initfunc', {
                             z: -1.2
                         });
                         console.log("This is a cafe object")
-                    } else if (obj.type == "restaurant") {
+                        this.el.sceneEl.appendChild(setObj(entity, model, obj, this.models));
+                    } else if (obj.properties.featuretype == "restaurant" && obj.properties.name != undefined) {
                         model.setAttribute('obj-model', {
-                            obj: this.models[obj.type][0],
-                            mtl: `${this.models[obj.type][0]}-mtl`
+                            obj: this.models[obj.properties.featuretype][0],
+                            mtl: `${this.models[obj.properties.featuretype][0]}-mtl`
                         });
                         console.log("This is a restaurant object")
-                    } else if (obj.type == "bar") {
+                        this.el.sceneEl.appendChild(setObj(entity, model, obj, this.models));
+                    } else if (obj.properties.featuretype == "bar" && obj.properties.name != undefined) {
                         model.setAttribute('obj-model', {
-                            obj: this.models[obj.type][0],
-                            mtl: `${this.models[obj.type][0]}-mtl`
+                            obj: this.models[obj.properties.featuretype][0],
+                            mtl: `${this.models[obj.properties.featuretype][0]}-mtl`
                         });
                         model.setAttribute('position', {
                             y: 2
                         });
                         console.log("This is a bar object")
+                        this.el.sceneEl.appendChild(setObj(entity, model, obj, this.models));
                     } else {
-                        model.setAttribute('obj-model', {
-                            obj: this.models[obj.type][0],
-                            mtl: `${this.models[obj.type][0]}-mtl`
-                        });
+                        console.log("Unknown type " + obj.properties.featuretype + " Or unknown name " + obj.properties.name)
                     }
-
-                    model.setAttribute('scale', {
-                        x: this.models[obj.type][1],
-                        y: this.models[obj.type][1],
-                        z: this.models[obj.type][1]
-                    });
-
-                    const sign = document.createElement('a-plane');
-                    sign.setAttribute('width', 5);
-                    sign.setAttribute('height', 2);
-                    sign.setAttribute('look-at', '[camera]');
-                    sign.setAttribute('material', {
-                        color: 'black'
-                    });
-                    sign.setAttribute('position', {
-                        x: 0,
-                        y: 5,
-                        z: 0
-                    });
-
-                    sign.setAttribute('text', {
-                        value: obj.name,
-                        wrapCount: 10,
-                        baseline: 'center'
-                    });
-
-                    entity.setAttribute('position', {
-                        x: obj.x,
-                        y: 0,
-                        z: obj.z
-                    });
-                    entity.appendChild(model);
-                    entity.appendChild(sign);
-                    model.addEventListener('click', e => {
-                        alert(obj.name);
-                        console.log("Object Clicked: " + obj.name)
-                    });
-
-
-                    this.el.sceneEl.appendChild(entity);
                 });
             }
         })
@@ -109,3 +63,50 @@ navigator.geolocation.watchPosition(pos => {
     e => { document.getElementById('position').innerHTML = `<strong>An error occurred: ${e}</strong>`; },
     { enableHighAccuracy: true, maximumAge: 5000 }
 );
+
+function setObj(entity, model, obj, models)
+{
+    console.log("Name: " + obj.properties.name + " X: " + obj.geometry.coordinates[0] + " Y: " + obj.geometry.coordinates[1] + " Type: " + obj.properties.featuretype)
+
+    model.setAttribute('scale', {
+        x: models[obj.properties.featuretype][1],
+        y: models[obj.properties.featuretype][1],
+        z: models[obj.properties.featuretype][1]
+    });
+
+    const sign = document.createElement('a-plane');
+    sign.setAttribute('width', 5);
+    sign.setAttribute('height', 2);
+    sign.setAttribute('look-at', '[camera]');
+    sign.setAttribute('material', {
+        color: 'black'
+    });
+    sign.setAttribute('position', {
+        x: 0,
+        y: 5,
+        z: 0
+    });
+
+    sign.setAttribute('text', {
+        value: obj.properties.name,
+        wrapCount: 10,
+        baseline: 'center'
+    });
+
+    entity.setAttribute('position', {
+        x: obj.geometry.coordinates[0],
+        y: 0,
+        z: obj.geometry.coordinates[1]
+    });
+    console.log("Setting X and Z to: " + obj.geometry.coordinates[0] + " " + obj.geometry.coordinates[1])
+
+    entity.appendChild(model);
+    entity.appendChild(sign);
+    model.addEventListener('click', e => {
+        alert(obj.properties.name);
+        console.log("Object Clicked: " + obj.properties.name)
+    });
+    console.log(entity)
+
+    return entity
+}
