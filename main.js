0949f9ceadb8b86959be3ef32937331fe60d8145
JoshuaAB98 <joshnash1998@gmail.com>
2020-12-24 18:00:26 +0000

    updated

--- main.js
+++ main.js
@@ -12,44 +12,200 @@ AFRAME.registerComponent('initfunc', {
                 };
 
                 // const res = await fetch('https://hikar.org/course/imm/world.php');
-                const res = await fetch(`https://www.hikar.org/fm/ws/bsvr.php?bbox=${e.detail.position.longitude-0.1},${e.detail.position.latitude-0.1},${e.detail.position.longitude+0.1},${e.detail.position.latitude+0.1}&outProj=4326&format=json&poi=all`);
+                const res = await fetch(`https://www.hikar.org/fm/ws/bsvr.php?bbox=${e.detail.position.longitude - 0.1},${e.detail.position.latitude - 0.1},${e.detail.position.longitude + 0.1},${e.detail.position.latitude + 0.1}&outProj=4326&format=json&poi=all`);
                 const json = await res.json();
 
                 json.features.forEach(obj => {
                     const entity = document.createElement('a-entity');
                     const model = document.createElement('a-entity');
 
-                    if (obj.properties.featuretype == "cafe" && obj.properties.name != undefined) {
+                    if (obj.properties.name != undefined) {
+                        objName = obj.properties.name
+                    }
+                    else {
+                        objName = "null"
+                    }
+
+                    console.log(objName.includes(objName))
+                    if (obj.properties.name != undefined && obj.properties.featuretype == undefined) {
+                        if (objName.includes("Cafe") == true) {
+                            type = "cafe"
+                        }
+                    } else {
+                        type = obj.properties.featuretype
+                    }
+
+                    console.log("Name: " + obj.properties.name + " Type: " + type)
+
+                    if (type == "cafe" && obj.properties.name != undefined) {
                         model.setAttribute('obj-model', {
-                            obj: this.models[obj.properties.featuretype][0],
-                            mtl: `${this.models[obj.properties.featuretype][0]}-mtl`
+                            obj: this.models[type][0],
+                            mtl: `${this.models[type][0]}-mtl`
                         });
                         model.setAttribute('position', {
                             x: -2.2,
                             y: 2.7,
                             z: -1.2
                         });
-                        console.log("This is a cafe object")
-                        this.el.sceneEl.appendChild(setObj(entity, model, obj, this.models));
-                    } else if (obj.properties.featuretype == "restaurant" && obj.properties.name != undefined) {
-                        model.setAttribute('obj-model', {
-                            obj: this.models[obj.properties.featuretype][0],
-                            mtl: `${this.models[obj.properties.featuretype][0]}-mtl`
+
+                        model.setAttribute('scale', {
+                            x: this.models[type][1],
+                            y: this.models[type][1],
+                            z: this.models[type][1]
                         });
-                        console.log("This is a restaurant object")
-                        this.el.sceneEl.appendChild(setObj(entity, model, obj, this.models));
-                    } else if (obj.properties.featuretype == "bar" && obj.properties.name != undefined) {
+
+                        const sign = document.createElement('a-plane');
+                        sign.setAttribute('width', 5);
+                        sign.setAttribute('height', 2);
+                        sign.setAttribute('look-at', '[camera]');
+                        sign.setAttribute('material', {
+                            color: 'black'
+                        });
+                        sign.setAttribute('position', {
+                            x: 0,
+                            y: 3,
+                            z: 0
+                        });
+
+                        sign.setAttribute('text', {
+                            value: obj.properties.name,
+                            wrapCount: 10,
+                            baseline: 'center'
+                        });
+
+                        entity.setAttribute('gps-projected-entity-place', {
+                            latitude: obj.geometry.coordinates[1],
+                            longitude: obj.geometry.coordinates[0]
+                        });
+
+                        entity.setAttribute('scale', {
+                            x: 10,
+                            y: 10,
+                            z: 10
+                        });
+
+                        entity.appendChild(model);
+                        entity.appendChild(sign);
+                        entity.addEventListener('click', e => {
+                            alert(obj.properties.name);
+                            console.log("Object Clicked: " + obj.properties.name)
+                        });
+
+                        this.el.sceneEl.appendChild(entity);
+                    }
+
+                    else if (type == "restaurant" && obj.properties.name != undefined) {
                         model.setAttribute('obj-model', {
-                            obj: this.models[obj.properties.featuretype][0],
-                            mtl: `${this.models[obj.properties.featuretype][0]}-mtl`
+                            obj: this.models[type][0],
+                            mtl: `${this.models[type][0]}-mtl`
+                        });
+                        model.setAttribute('scale', {
+                            x: this.models[type][1],
+                            y: this.models[type][1],
+                            z: this.models[type][1]
+                        });
+
+                        model.setAttribute('position', {
+                            y: -1.6
+                        });
+
+                        const sign = document.createElement('a-plane');
+                        sign.setAttribute('width', 5);
+                        sign.setAttribute('height', 4);
+                        sign.setAttribute('look-at', '[camera]');
+                        sign.setAttribute('material', {
+                            color: 'black'
+                        });
+                        sign.setAttribute('position', {
+                            x: 0,
+                            y: 3,
+                            z: 0
+                        });
+
+                        sign.setAttribute('text', {
+                            value: obj.properties.name,
+                            wrapCount: 10,
+                            baseline: 'center'
+                        });
+
+                        entity.setAttribute('gps-projected-entity-place', {
+                            latitude: obj.geometry.coordinates[1],
+                            longitude: obj.geometry.coordinates[0]
+                        });
+
+                        entity.setAttribute('scale', {
+                            x: 10,
+                            y: 10,
+                            z: 10
+                        });
+
+                        entity.appendChild(model);
+                        entity.appendChild(sign);
+                        entity.addEventListener('click', e => {
+                            alert(obj.properties.name);
+                            console.log("Object Clicked: " + obj.properties.name)
+                        });
+
+                        this.el.sceneEl.appendChild(entity);
+                    }
+
+                    else if (type == "bar" || type == "pub" && obj.properties.name != undefined) {
+                        model.setAttribute('obj-model', {
+                            obj: this.models["bar"][0],
+                            mtl: `${this.models["bar"][0]}-mtl`
                         });
                         model.setAttribute('position', {
-                            y: 2
+                            y: 0
                         });
-                        console.log("This is a bar object")
-                        this.el.sceneEl.appendChild(setObj(entity, model, obj, this.models));
-                    } else {
-                        console.log("Unknown type " + obj.properties.featuretype + " Or unknown name " + obj.properties.name)
+                        model.setAttribute('scale', {
+                            x: this.models["bar"][1],
+                            y: this.models["bar"][1],
+                            z: this.models["bar"][1]
+                        });
+
+                        const sign = document.createElement('a-plane');
+                        sign.setAttribute('width', 5);
+                        sign.setAttribute('height', 2);
+                        sign.setAttribute('look-at', '[camera]');
+                        sign.setAttribute('material', {
+                            color: 'black'
+                        });
+                        sign.setAttribute('position', {
+                            x: 0,
+                            y: 3,
+                            z: 0
+                        });
+
+                        sign.setAttribute('text', {
+                            value: obj.properties.name,
+                            wrapCount: 10,
+                            baseline: 'center'
+                        });
+
+                        entity.setAttribute('gps-projected-entity-place', {
+                            latitude: obj.geometry.coordinates[1],
+                            longitude: obj.geometry.coordinates[0]
+                        });
+
+                        entity.setAttribute('scale', {
+                            x: 10,
+                            y: 10,
+                            z: 10
+                        });
+
+                        entity.appendChild(model);
+                        entity.appendChild(sign);
+
+                        entity.addEventListener('click', e => {
+                            alert(obj.properties.name);
+                            console.log("Object Clicked: " + obj.properties.name)
+                        });
+
+                        this.el.sceneEl.appendChild(entity);
+                    }
+
+                    else {
+                        console.log("Type " + type + " Name " + obj.properties.name)
                     }
                 });
             }
@@ -64,49 +220,3 @@ navigator.geolocation.watchPosition(pos => {
     { enableHighAccuracy: true, maximumAge: 5000 }
 );
 
-function setObj(entity, model, obj, models)
-{
-    console.log("Name: " + obj.properties.name + " X: " + obj.geometry.coordinates[0] + " Y: " + obj.geometry.coordinates[1] + " Type: " + obj.properties.featuretype)
-
-    model.setAttribute('scale', {
-        x: models[obj.properties.featuretype][1],
-        y: models[obj.properties.featuretype][1],
-        z: models[obj.properties.featuretype][1]
-    });
-
-    const sign = document.createElement('a-plane');
-    sign.setAttribute('width', 5);
-    sign.setAttribute('height', 2);
-    sign.setAttribute('look-at', '[camera]');
-    sign.setAttribute('material', {
-        color: 'black'
-    });
-    sign.setAttribute('position', {
-        x: 0,
-        y: 5,
-        z: 0
-    });
-
-    sign.setAttribute('text', {
-        value: obj.properties.name,
-        wrapCount: 10,
-        baseline: 'center'
-    });
-
-    entity.setAttribute('position', {
-        x: obj.geometry.coordinates[0],
-        y: 0,
-        z: obj.geometry.coordinates[1]
-    });
-    console.log("Setting X and Z to: " + obj.geometry.coordinates[0] + " " + obj.geometry.coordinates[1])
-
-    entity.appendChild(model);
-    entity.appendChild(sign);
-    model.addEventListener('click', e => {
-        alert(obj.properties.name);
-        console.log("Object Clicked: " + obj.properties.name)
-    });
-    console.log(entity)
-
-    return entity
-}
