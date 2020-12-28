d35471cf0bbc8e73c5aec91a878ba76696ed8f0f
JoshuaAB98 <joshnash1998@gmail.com>
2020-12-28 10:39:54 +0000

    Not so fucked

--- main.js
+++ main.js
@@ -268,21 +268,17 @@ if ('serviceWorker' in navigator) {
 }
 
 let deferredPrompt;
-const addBtn = document.getElementById('add-button');
+
 
 window.addEventListener('beforeinstallprompt', (e) => {
-
+const addBtn = document.getElementById('add-button');
     console.log("User has been prompted to install")
     // Prevent Chrome 67 and earlier from automatically showing the prompt
     e.preventDefault();
     // Stash the event so it can be triggered later.
     deferredPrompt = e;
-    // Update UI to notify the user they can add to home screen
-    addBtn.style.display = 'block';
 
     addBtn.addEventListener('click', (e) => {
-        // hide our user interface that shows our A2HS button
-        addBtn.style.display = 'none';
         // Show the prompt
         deferredPrompt.prompt();
         // Wait for the user to respond to the prompt
